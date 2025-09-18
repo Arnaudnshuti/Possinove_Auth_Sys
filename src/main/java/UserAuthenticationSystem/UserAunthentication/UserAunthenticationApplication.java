ef6b629cc0827845fe70888a36d405db3596a888
Arnaudnshuti <arnaudnshutidjadja@gmail.com>
2025-09-18 12:22:06 +0200

    Integrated React frontend into Spring Boot

--- src/main/java/UserAuthenticationSystem/UserAunthentication/UserAunthenticationApplication.java
+++ src/main/java/UserAuthenticationSystem/UserAunthentication/UserAunthenticationApplication.java
@@ -2,9 +2,6 @@ package UserAuthenticationSystem.UserAunthentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UserAunthenticationApplication {
@@ -13,15 +10,4 @@ public class UserAunthenticationApplication {
         SpringApplication.run(UserAunthenticationApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://possinove-auth-sys-h8i5.vercel.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
-            }
-        };
-    }
 }
